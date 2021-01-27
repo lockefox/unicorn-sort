4b5e3812c3f9f2ee8092e9465bcebca5d9f3efef
lockefox <jpurcell.ee@gmail.com>
2021-01-26 19:12:00 -0800

    adding basic README + setup

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # unicorn-sort
 A tool for sorting and backing up RAW photo files between a drive/NAS
+
+## Setup
+
+CLI installable, or docker runable.  Designed and tested on a mac.
+
+```bash
+unicorn --source-dir=... --stars=... --nfs=...
+```
+
+Point unicorn at a volume mount, give it filter criteria, and a place to backup.  `unicorn` will then:
+1) copy all files whos EXIF data matches the `stars` criteria
+2) copy `source-dir` to a nfs or smb location
+
+`unicorn` is a dumb script made for personal use.  Will stomp files if your naming scheme isn't unique.
---
+++ setup.py
@@ -0,0 +1,29 @@
+"""package setup for unicorn-sort"""
+from codecs import open
+from pathlib import Path
+from setuptools import setup, find_packages
+
+__package_name__ = "unicorn-sort"
+__library_name__ = "unicorn_sort"
+HERE = Path(__file__).resolve().parent
+
+with open("README.md", "r", "utf-8") as f:
+    __readme__ = f.read()
+
+with open(HERE / __library_name__ / "VERSION", "r", "utf-8") as f:
+    __version__ = f.read()
+
+setup(
+    name=__package_name__,
+    description="Helper for sorting and backing up photo file dumps",
+    version=__version__,
+    long_description=__readme__,
+    long_description_content_type="text/markdown",
+    author="John Purcell",
+    author_email="jpurcell.ee@gmail.com",
+    packages=find_packages(),
+    package_data={"": ["README"], __library_name__: ["VERSION"],},
+    entry_points={"console_scripts": [f"unicorn={__library_name__}.cli:run",]},
+    install_requires=["plumbum",],
+    extras_require={"dev": {"tox", "tox-travis", "black",}},
+)
